4aa1e1c036cad3b9414ff125f46db8bf8fa8750d
Foiering Sergey <serjofoi@gmail.com>
2022-09-05 20:05:15 +0300

    Finished css plugin and removed esbuild.wasm file

--- src/plugins/fetch-plugin.ts
+++ src/plugins/fetch-plugin.ts
@@ -10,34 +10,48 @@ export const fetchPlugin = (inputCode: string) => {
   return {
     name: 'fetch-plugin',
     setup(build: esbuild.PluginBuild) {
+      build.onLoad({ filter: /(^index\.js$)/ }, () => {
+        return {
+          loader: 'jsx',
+          contents: inputCode,
+        };
+      });
+
       build.onLoad({ filter: /.*/ }, async (args: any) => {
-        if (args.path === 'index.js') {
-          return {
-            loader: 'jsx',
-            contents: inputCode,
-          };
+        const cachedResult = await fileCache.getItem<esbuild.OnLoadResult>(args.path);
+        if (cachedResult) {
+          return cachedResult;
         }
-        // const cachedResult = await fileCache.getItem<esbuild.OnLoadResult>(args.path);
-        // if (cachedResult) {
-        //   return cachedResult;
-        // }
+      });
 
+      build.onLoad({ filter: /.css$/ }, async (args: any) => {
         const { data, request } = await axios.get(args.path);
-        //* responseURL include the path file name at the end.
-        //TODO: resolve the path name included at the end, required the path without the file name at the end.
 
-        const fileType = args.path.match(/.css$/) ? 'css' : 'jsx';
-        const contents =
-          fileType === 'css'
-            ? ` 
+        const escaped = data.replace(/\n/g, '').replace(/"/g, '\\"').replace(/'/g, "\\'");
+        const contents = ` 
           const style = document.createElement('style'); 
-          style.innerText = 'body { background-color: "green"}'; 
-          document.head.appendChild(style)`
-            : data;
+          style.innerText = '${escaped}'; 
+          document.head.appendChild(style)`;
 
         const result: esbuild.OnLoadResult = {
           loader: 'jsx',
           contents,
+          resolveDir: new URL('./', request.responseURL).pathname,
+        };
+
+        //* store response in cache indexedDB.
+        await fileCache.setItem(args.path, result);
+        return result;
+      });
+
+      build.onLoad({ filter: /.*/ }, async (args: any) => {
+        const { data, request } = await axios.get(args.path);
+        //* responseURL include the path file name at the end.
+        //TODO: resolve the path name included at the end, required the path without the file name at the end.
+
+        const result: esbuild.OnLoadResult = {
+          loader: 'jsx',
+          contents: data,
           resolveDir: new URL('./', request.responseURL).pathname,
         };
 
