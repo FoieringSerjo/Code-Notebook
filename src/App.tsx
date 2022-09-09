2ff77b39a78eeab9bc0ed2c6f9034968beee875a
Foiering Sergey <serjofoi@gmail.com>
2022-09-09 11:40:47 +0300

    Adding code editor and styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
+import 'bulmaswatch/superhero/bulmaswatch.min.css';
 import * as esbuild from 'esbuild-wasm';
 import { useState, useEffect, useRef } from 'react';
 import { unpkgPathPlugin } from './plugins/unpkg-path-plugin';
 import { fetchPlugin } from './plugins/fetch-plugin';
+import CodeEditor from './components/code-editor';
 
 const App = () => {
   const ref = useRef<any>();
@@ -73,6 +75,7 @@ const App = () => {
 
   return (
     <div>
+      <CodeEditor initialValue="const a = 1;" onChange={(value) => setInput(value)} />
       <textarea value={input} onChange={(e) => setInput(e.target.value)}></textarea>
       <div>
         <button onClick={onClick}>Submit</button>
