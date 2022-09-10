5846077172897e5a0adedfd9a0532e8709136e93
Foiering Sergey <serjofoi@gmail.com>
2022-09-10 18:31:47 +0300

    white background as workaround in preview file

--- src/components/preview.tsx
+++ src/components/preview.tsx
@@ -13,7 +13,9 @@ interface PreviewProps {
 //   `;
 const html = `
 <html>
-  <head></head>
+  <head>
+    <style>html { background-color: white; }</style>
+  </head>
   <body>
     <div id="root"></div>
     <script>
